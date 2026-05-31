0749cc1a92573efbd375f6e14be654eef5047361
Yogesh <yg88385@gmail.com>
2026-05-31 08:41:00 +0530

    c  debug

--- server/controllers/contactController.js
+++ server/controllers/contactController.js
@@ -1,7 +1,29 @@
-export const sendContactMail = async (req, res) => {
-  console.log("CONTACT API HIT");
+import nodemailer from "nodemailer";
 
-  return res.status(200).json({
-    message: "API Working",
-  });
+export const sendContactMail = async (req, res) => {
+  try {
+    console.log("EMAIL_USER =", process.env.EMAIL_USER);
+    console.log("EMAIL_PASS =", process.env.EMAIL_PASS ? "FOUND" : "NOT FOUND");
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.verify();
+
+    return res.status(200).json({
+      message: "SMTP Connected",
+    });
+
+  } catch (error) {
+    console.log("FULL ERROR:", error);
+
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 };
