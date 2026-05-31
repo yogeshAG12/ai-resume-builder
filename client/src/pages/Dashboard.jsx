c220c64de73fd2e99c43857d06302904996e2116
Yogesh <yg88385@gmail.com>
2026-06-01 03:30:29 +0530

    name added

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -96,7 +96,7 @@ const uploadResume = async (event) => {
     <div>
       <div className='max-w-7xl mx-auto px-4 py-8'>
 
-        <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome,</p>
+        <p className='text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden'>Welcome,  {user?.name}</p>
 
         <div className='grid grid-cols-2 sm:flex gap-4'>
             <button onClick={()=> setShowCreateResume(true)} className='w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer'>
