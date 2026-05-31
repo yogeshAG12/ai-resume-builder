d17433671eda68c5c3d128b609550071125c3ada
Yogesh <yg88385@gmail.com>
2026-05-31 09:02:16 +0530

    Added reference

--- client/src/components/home/Hero.jsx
+++ client/src/components/home/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
             <div className="min-h-screen pb-20">
                 {/* Navbar */}
                 <nav className="z-50 flex items-center justify-between w-full py-4 px-6 md:px-16 lg:px-24 xl:px-40 text-sm">
-                    <a href="https://prebuiltui.com">
+                    <a href="#">
                         <img src="/logo.svg" alt="logo" className="h-11 w-auto" />
                     </a>
 
